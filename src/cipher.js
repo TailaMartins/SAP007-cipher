57336e2108e2b4a262ca10162966e53faa83aea0
Taila Martins <tailamartins74@gmail.com>
2022-01-26 13:47:29 -0300

    cifra foi feita

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,53 @@
 const cipher = {
-  // ...
-};
+    encode(offset, string) {
+        if(typeof offset != 'number' || (offset != 0 && offset % 1 != 0)){
+            throw new TypeError();
+        }
+        if (typeof string != 'string') {
+            throw new TypeError();
+
+        }
+
+        let mensagemSecreta = "";
+        for (let i = 0; i < string.length; i++) {
+            const element = string[i];
+            let codDaletraaASC = element.charCodeAt(0);
+            let retornoEncode = ((codDaletraaASC - 65 + offset) % 26 + 65);
+            let novaLetra = String.fromCharCode(retornoEncode);
+            mensagemSecreta = mensagemSecreta + novaLetra;
+        }
+
+        return mensagemSecreta;
+
+    },
+    decode(offset, string) {
+        if(typeof offset != 'number' || (offset != 0 && offset % 1 != 0)){
+            throw new TypeError();
+        }
+        if (typeof string != 'string') {
+            throw new TypeError();
+
+        }
+        let mensagemSecreta = "";
+        for (let i = 0; i < string.length; i++) {
+            const element = string[i];
+            let codDaletraaASC = element.charCodeAt(0);
+            let retornoDecode = ((codDaletraaASC - 90 - offset) % 26 + 90);
+            let novaLetra = String.fromCharCode(retornoDecode);
+            mensagemSecreta = mensagemSecreta + novaLetra;
+        }
+
+        return mensagemSecreta;
+
+
+    }
+
+}
+    
 
 export default cipher;
+
+// ((codDaletraaASC - codDa1letra + deslocamento) % tamanhodoAlfabeto + codDa1letra)
+
+
+ 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-import cipher from './cipher.js';
+//  import cipher from './cipher.js';
 
-console.log(cipher);
+
+// console.log(cipher);
+
+// let input = document.querySelector(".enviar");
+
+// input.addEventListener('click');
+
+
+
+
+
+
