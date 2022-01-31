18075e777b8b238761eac8a949c2c9ed21e24999
Taila Martins <tailamartins74@gmail.com>
2022-01-31 02:06:05 -0300

    alguns caracteres especiais e alteração no css

--- src/cipher.js
+++ src/cipher.js
@@ -5,7 +5,6 @@ const cipher = {                                                                
         }
         if (typeof string != 'string') {                                                            /* aqui tambem é pra dar erro se usuario colocar outro tipo de argumento */
             throw new TypeError();                                                                  /* aqui é o erro que também está vazio*/
-
         }
 
         let mensagemSecreta = "";                                                              /* aqui é a minha variavel */
@@ -13,25 +12,37 @@ const cipher = {                                                                
             const quantidaDeletras = string[i];                                                       /* aqui e o meu for com uma const que recebe minha string o[i] é oindice da minha const que vai ser verificado */
             let codDaletraaASC = quantidaDeletras.charCodeAt(0);
 
-
             if (codDaletraaASC >= 65 && codDaletraaASC <= 90) {
                 let retornoEncode = ((codDaletraaASC - 65 + offset) % 26 + 65);
                 let novaLetra = String.fromCharCode(retornoEncode);
-
                 mensagemSecreta = mensagemSecreta + novaLetra;
 
-            } if (codDaletraaASC >= 97 && codDaletraaASC <= 122) {
+            } else if (codDaletraaASC >= 97 && codDaletraaASC <= 122) {
                 let retornoEncode = ((codDaletraaASC - 97 + offset) % 26 + 97);
                 let novaLetra = String.fromCharCode(retornoEncode);
-
                 mensagemSecreta = mensagemSecreta + novaLetra;
 
+            } else if (codDaletraaASC >= 32 && codDaletraaASC <= 47) {
+                let retornoEncode = ((codDaletraaASC - 32 + offset) % 26 + 32);
+                let novaLetra = String.fromCharCode(retornoEncode);
+                mensagemSecreta = mensagemSecreta + novaLetra;
+
+            } else if(codDaletraaASC >= 58 && codDaletraaASC <= 64) {
+                let retornoEncode = ((codDaletraaASC - 58 + offset) % 26 + 64);
+                let novaLetra = String.fromCharCode(retornoEncode);
+                mensagemSecreta = mensagemSecreta + novaLetra;
+
+                
+            } else if (codDaletraaASC >= 123 && codDaletraaASC <= 254) {
+                let retornoEncode = ((codDaletraaASC - 123 + offset) % 26 + 123);
+                let novaLetra = String.fromCharCode(retornoEncode);
+                mensagemSecreta = mensagemSecreta + novaLetra;
+
+            } else{
+                alert("Digite sua mensagem");
             }
-
         }
-
         return mensagemSecreta;
-
     },
     decode(offset, string) {
         if (typeof offset != 'number' || (offset != 0 && offset % 1 != 0)) {
@@ -39,8 +50,8 @@ const cipher = {                                                                
         }
         if (typeof string != 'string') {
             throw new TypeError();
-
         }
+
         let mensagemCodificada = "";
         for (let i = 0; i < string.length; i++) {
             const quantidaDeletras = string[i];
@@ -49,26 +60,35 @@ const cipher = {                                                                
             if (codDaletraaASC >= 65 && codDaletraaASC <= 90) {
                 let retornoDecode = ((codDaletraaASC - 90 - offset) % 26 + 90);
                 let novaLetra = String.fromCharCode(retornoDecode);
-
                 mensagemCodificada = mensagemCodificada + novaLetra;
 
-            } if (codDaletraaASC >= 97 && codDaletraaASC <= 122) {
+            } else if (codDaletraaASC >= 97 && codDaletraaASC <= 122) {
                 let retornoDecode = ((codDaletraaASC - 122 - offset) % 26 + 122);
                 let novaLetra = String.fromCharCode(retornoDecode);
                 mensagemCodificada = mensagemCodificada + novaLetra;
 
-            }
+            } else if (codDaletraaASC >= 32 && codDaletraaASC <= 47) {
+                let retornoDecode = ((codDaletraaASC - 47 - offset) % 26 + 47);
+                let novaLetra = String.fromCharCode(retornoDecode);
+                mensagemCodificada = mensagemCodificada + novaLetra;
 
+            } else if(codDaletraaASC >= 58 && codDaletraaASC <= 64){
+                let retornoDecode = ((codDaletraaASC - 64 - offset) % 26 + 64);
+                let novaLetra = String.fromCharCode(retornoDecode);
+                mensagemCodificada = mensagemCodificada + novaLetra;
+
+            } else if (codDaletraaASC >=123 && codDaletraaASC <= 254) {
+                let retornoDecode = ((codDaletraaASC - 254 - offset) % 26 + 254);
+                let novaLetra = String.fromCharCode(retornoDecode);
+                mensagemCodificada = mensagemCodificada + novaLetra;
+
+            } else{
+                alert("Digite sua mensagem !!!")
+            }               
         }
-
         return mensagemCodificada;
-
-
     }
-
 }
-
-
 export default cipher;
 
 // ((codDaletraaASC - codDa1letra + deslocamento) % tamanhodoAlfabeto + codDa1letra)
