075ebcd08e882f3414c75a7c4cd8db748bbaf492
Taila Martins <tailamartins74@gmail.com>
2022-02-04 13:04:41 -0300

    alterações

--- src/cipher.js
+++ src/cipher.js
@@ -1,50 +1,29 @@
-//const cipher é um objeto pois tem propriedades e definições
 const cipher = {
     encode(offset, string) {
-        //encode é a função que recebe um offset e uma string
         if (typeof offset != 'number' || (offset != 0 && offset % 1 != 0)) {
-            //if é uma condição; tyeof é o tipo ; !=0 é um operador logico de negação 
             throw new TypeError();
-            //throw ele tipo joga um erro
         }
         if (typeof string != 'string') {
             throw new TypeError();
-            //é onde vai dar erro,()deixei vazio pois não me senti segura de escrever o erro
         }
         let mensagemSecreta = "";
         for (let i = 0; i < string.length; i++) {
             const quantidaDeletras = string[i];
-            let codDaletraaASC = quantidaDeletras.charCodeAt(0);
-            //laço for que contem um contador e variavel* que vai verficar o que o usuario 
-            //escreve e transformar em numeraçao ASCii
-            
-            if (codDaletraaASC >= 65 && codDaletraaASC <= 90) {
-                let retornoEncode = ((codDaletraaASC - 65 + offset) % 26 + 65);
-                let novaLetra = String.fromCharCode(retornoEncode);
+            const codDaletraaASC = quantidaDeletras.charCodeAt(0);
+
+            if (codDaletraaASC >= letraAmaiscula && codDaletraaASC <= letraZmaiscula) {
+                const retornoEncode = ((codDaletraaASC - letraAmaiscula + offset) % tamanhodoAlfabeto + letraAmaiscula);
+                const novaLetra = String.fromCharCode(retornoEncode);
                 mensagemSecreta = mensagemSecreta + novaLetra;
 
-            } else if (codDaletraaASC >= 97 && codDaletraaASC <= 122) {
-                let retornoEncode = ((codDaletraaASC - 97 + offset) % 26 + 97);
-                let novaLetra = String.fromCharCode(retornoEncode);
+            } else if (codDaletraaASC >= letraAminuscula && codDaletraaASC <= letraZminuscula) {
+                const retornoEncode = ((codDaletraaASC - letraAminuscula + offset) % tamanhodoAlfabeto + letraAminuscula);
+                const novaLetra = String.fromCharCode(retornoEncode);
                 mensagemSecreta = mensagemSecreta + novaLetra;
 
-            // Aqui ele vai vericar os caracteres especiais, ele não vai cifrar, apenas vai 
-            //permitir que o usuario digite com os caracteres.
-            } else if (codDaletraaASC >= 32 && codDaletraaASC <= 47) {
-                let novaLetra = String.fromCharCode(codDaletraaASC);
+            } else {
+                const novaLetra = String.fromCharCode(codDaletraaASC);
                 mensagemSecreta = mensagemSecreta + novaLetra;
-
-            } else if (codDaletraaASC >= 58 && codDaletraaASC <= 64) {
-                let novaLetra = String.fromCharCode(codDaletraaASC);
-                mensagemSecreta = mensagemSecreta + novaLetra;
-
-            } else if(codDaletraaASC >=123 && codDaletraaASC <= 254){
-                let novaLetra = String.fromCharCode(codDaletraaASC);
-                mensagemSecreta = mensagemSecreta + novaLetra;
-
-            //Se o usuario digitar qualquer caractere que não esteja na condição(if e else) irá pedir que digite a mensagem
-            } else {  
-                alert(mensagemSecreta = "Digite sua mensagem");
             }
         }
         return mensagemSecreta;
@@ -59,31 +38,21 @@ const cipher = {
         let mensagemCodificada = "";
         for (let i = 0; i < string.length; i++) {
             const quantidaDeletras = string[i];
-            let codDaletraaASC = quantidaDeletras.charCodeAt(0);
+            const codDaletraaASC = quantidaDeletras.charCodeAt(0);
 
-            if (codDaletraaASC >= 65 && codDaletraaASC <= 90) {
-                let retornoDecode = ((codDaletraaASC - 90 - offset) % 26 + 90);
-                let novaLetra = String.fromCharCode(retornoDecode);
-                mensagemCodificada = mensagemCodificada + novaLetra;
-
-            } else if (codDaletraaASC >= 97 && codDaletraaASC <= 122) {
-                let retornoDecode = ((codDaletraaASC - 122 - offset) % 26 + 122);
-                let novaLetra = String.fromCharCode(retornoDecode);
-                mensagemCodificada = mensagemCodificada + novaLetra;
-                
-            } else if (codDaletraaASC >= 32 && codDaletraaASC <= 47) {
-                let novaLetra = String.fromCharCode(codDaletraaASC);
+            if (codDaletraaASC >= letraAmaiscula && codDaletraaASC <= letraZmaiscula) {
+                const retornoDecode = ((codDaletraaASC - letraZmaiscula - offset) % tamanhodoAlfabeto + letraZmaiscula);
+                const novaLetra = String.fromCharCode(retornoDecode);
                 mensagemCodificada = mensagemCodificada + novaLetra;
 
-            } else if (codDaletraaASC >= 58 && codDaletraaASC <= 64) {
-                let novaLetra = String.fromCharCode(codDaletraaASC);
+            } else if (codDaletraaASC >= letraAminuscula && codDaletraaASC <= letraZminuscula) {
+                const retornoDecode = ((codDaletraaASC - letraZminuscula - offset) % tamanhodoAlfabeto + letraZminuscula);
+                const novaLetra = String.fromCharCode(retornoDecode);
                 mensagemCodificada = mensagemCodificada + novaLetra;
 
-            }else if(codDaletraaASC >= 123 && codDaletraaASC <= 254){
-                let novaLetra = String.fromCharCode(codDaletraaASC);
+            } else {
+                const novaLetra = String.fromCharCode(codDaletraaASC);
                 mensagemCodificada = mensagemCodificada + novaLetra;
-            }else {
-                alert(mensagemCodificada = "Digite sua mensagem");
             }
         }
         return mensagemCodificada;
@@ -91,10 +60,9 @@ const cipher = {
 }
 export default cipher;
 
-// ((codDaletraaASC - codDa1letra + deslocamento) % tamanhodoAlfabeto + codDa1letra)
-// variavel ela armazena uma informação que eu preciso
-// let = pode mudar a informação
-// const = não muda
-
-
+const letraAmaiscula = 65;
+const letraZmaiscula = 90;
+const tamanhodoAlfabeto = 26;
+const letraAminuscula = 97;
+const letraZminuscula = 122;
 
